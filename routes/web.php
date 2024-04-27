26bb379438195adacdf38e3320691ae977636302
unknown <elidovrich.coding@gmail.com>
2024-04-26 20:43:21 -0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pizzas', function () {
+    //1. get data from database
+    //2. then pass the data into the view (page) via an array of key-value pairs
+    $pizza = ['type' => 'hawaiian', 'base' => 'cheesy crust', 'price' => '10'];
+    return view('pizzas', $pizza);
+});
