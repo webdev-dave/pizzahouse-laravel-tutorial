c223a1b27f5d85d44f389005fe47538b372bff1d
unknown <elidovrich.coding@gmail.com>
2024-04-29 13:50:51 -0700

    18-creating a webform

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -1,12 +1,26 @@
 @extends('layouts.layout');
 
 @section('content')
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title m-b-md">
-           Add A New Pizza 
-        </div>
+    <div class="wrapper create-pizza">
+        <h1>Create a New Pizza</h1>
+        <form action="/pizzas" method="post">
+            <label for="name">Your name:</label>
+            <input type="text" id="name" name="name">
+            <label for="type">Choose pizza type:</label>
+            <select name="type" id="type">
+                <option value="margherita">Margherita</option>
+                <option value="hawaiian">Hawaiian</option>
+                <option value="veg supreme">Veg Supreme</option>
+                <option value="volcano">Volcano</option>
+            </select>
+            <label for="base">Choose pizza type:</label>
+            <select name="base" id="type">
+                <option value="cheesy crust">Cheesy Crust</option>
+                <option value="garlic crust">Carlic Crust</option>
+                <option value="thin & crispy">Thin & Crispy</option>
+                <option value="thick">Thick</option>
+            </select>
+            <input type="submit" value="Order Pizza">
+        </form>
     </div>
-</div>
-
 @endsection
