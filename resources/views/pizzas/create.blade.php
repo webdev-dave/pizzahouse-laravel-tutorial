ceb232a093c6c561e5fa03e47a8aa588260836d1
unknown <elidovrich.coding@gmail.com>
2024-04-29 16:12:51 -0700

    19-post requests

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <div class="wrapper create-pizza">
         <h1>Create a New Pizza</h1>
-        <form action="/pizzas" method="post">
+        <form action="/pizzas" method="POST">
+            @csrf
             <label for="name">Your name:</label>
             <input type="text" id="name" name="name">
             <label for="type">Choose pizza type:</label>
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('/', function () {
 
 Route::get('/pizzas', [PizzaController::class, 'index']);
 Route::get('/pizzas/create', [PizzaController::class,'create']);
+Route::post('/pizzas', [PizzaController::class,'store']);
 Route::get('/pizzas/{id}', [PizzaController::class, 'show']);
 
 
