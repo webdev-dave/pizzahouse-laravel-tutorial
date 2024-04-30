b737cf449eaaa8b471461311ff64b1e7f73719b0
unknown <elidovrich.coding@gmail.com>
2024-04-30 14:58:47 -0700

    add sass using vite

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,12 +5,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Pizza House</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <!-- Styles -->
-    <link rel="stylesheet" href="/css/main.css">
+    @vite(['resources/css/app.css', 'resources/scss/app.scss', 'resources/css/main.css', 'resources/scss/main.scss'])
+    {{-- <link rel="stylesheet" href="/css/main.css"> --}}
 </head>
 
 <body>
