4054492d192c3d939d8a8382edc4a17d5a0db589
unknown <elidovrich.coding@gmail.com>
2024-04-30 18:42:46 -0700

    27-disabling registration

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,17 @@ Route::get('/', function () {
 });
 
 Route::get('/pizzas', [PizzaController::class, 'index'])->middleware('auth');
-Route::get('/pizzas/{id}', [PizzaController::class, 'show'])->middleware('auth');
 Route::get('/pizzas/create', [PizzaController::class,'create']);
 Route::post('/pizzas', [PizzaController::class,'store']);
+Route::get('/pizzas/{id}', [PizzaController::class, 'show'])->middleware('auth');
 Route::delete('/pizzas/{id}', [PizzaController::class,'destroy'])->middleware('auth');
 
 
 
+Auth::routes([
+    //use this code if you want to prevent/disable registration
+    // 'register' => false,
+]);
 
-
-
-Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
